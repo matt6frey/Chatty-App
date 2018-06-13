4ef085968de527319f2edf6e58bf879daabd265d
Matt Frey <matt6frey@gmail.com>
2018-06-13 19:19:52 +0000

    Commiting all major changes. Setting up Web Sockets Coms

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,116 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Nav from './nav.jsx';
+import msg from './messagelist.js';
+import Chatbar from './chatbar.jsx';
 
-class App extends Component {
+class Main extends Component {
+  constructor (props) {
+    super(props);
+  }
+
   render() {
+
+    const makeId = () => {
+      const vowels = ['a','e','i','o','u'];
+      let i = 0;
+      let id = '';
+      while (i < 6) {
+        id += Math.ceil(Math.random()*9).toString() + vowels[Math.ceil(Math.random()*4)];
+        i++;
+      }
+      return id;
+    };
+
+    const msgFeed = this.props.messages.map( function (msg) {
+      let id = makeId();
+      if(msg.type === 'incomingNotification') {
+        return <div key={id} className="message system">
+                {msg.content}
+              </div>;
+      } else {
+        return <div key={id} className="message">
+                <span className="message-username">{msg.username}</span>
+                <span className="message-content">{msg.content}</span>
+              </div>;
+      }
+    });
+
+
     return (
-      <h1>Hello React :)</h1>
+      <main className="messages">
+          {msgFeed}
+      </main>
     );
   }
 }
+
+
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = { user: 'Anonymous', messages: msg };
+    this.addMessage = this.addMessage.bind(this);
+    this.Notification = this.Notification.bind(this);
+  }
+
+  addMessage (name, msg) {
+    // Set to anonynous if name is empty
+    if(name === '') { name = "Anonymous" }
+    // Add message if message isn't empty
+    if(msg !== '') {
+        const newMessage = {username: name, content: msg, type: "incomingMessage"};
+        let messages = this.state.messages;
+        messages = messages.concat(newMessage);
+        this.setState({messages: messages});
+        this.socket.send(JSON.stringify(newMessage));
+    } else {
+      return false;
+    }
+  };
+
+  Notification (oldName, newName) {
+    // Check for old vs new name, if different: show notification.
+    if(newName !== oldName) {
+    const newMessage = {username: name, content: `${oldName} changed their name to ${newName}.`, type: "incomingNotification"};
+    let messages = this.state.messages;
+    this.socket.send(JSON.stringify(newMessage));
+    messages = messages.concat(newMessage);
+    this.setState({user: newName, messages: messages});
+
+    }
+  }
+
+  componentDidMount() {
+    // Connect to WS
+    this.socket = new WebSocket('ws:localhost:3001/ws', 'protocolOne');
+  }
+
+
+  render() {
+    const data = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    };
+
+    return (
+      <div>
+        <Nav />
+        <Main messages={ this.state.messages } />
+        <Chatbar user={ this.state.user } addMessage={ this.addMessage.bind(this) } Notification={ this.Notification.bind(this) } />
+      </div>
+    );
+  }
+}
+
 export default App;
---
+++ src/chatbar.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+
+class Chatbar extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    let currentUser =  props.user;
+    this.state = { user: currentUser, addMessage: props.addMessage, Notification: props.Notification };
+  }
+
+  render() {
+  const addMessage = this.state.addMessage;
+  const Notification = this.state.Notification;
+  const user = this.props.user;
+    return (
+      <footer className="chatbar" onKeyUp={ (e) => {
+        if (e.key === 'Enter') {
+          // get info from Chat Box
+          let name = (e.target.className.search('username') != -1) ? e.target.value : e.target.parentNode.children[0].value;
+          let msg = (e.target.className.search('message') != -1) ? e.target.value : e.target.parentNode.children[1].value;
+          let type = (name === name) ? "incomingMessage" : "incomingNotification";
+          addMessage(name, msg);
+        } } }>
+        <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={ this.state.user } onBlur={
+          (e) => { Notification(user, e.target.value); } } />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      </footer>
+    );
+  }
+}
+
+export default Chatbar;
